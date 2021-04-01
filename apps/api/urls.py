c80efa07ec959ca28b15be37b44f0c84b9a190e0
Thiago Brasil <tchaguitos@gmail.com>
2021-04-01 17:49:52 -0300

    feat: add endpoint para listar cidades

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,7 +1,22 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from locais.viewsets import CidadeViewSet
+from cadastro.viewsets import CadastroViewSet
+
 router = routers.DefaultRouter()
+
+router.register(
+    "cidades",
+    CidadeViewSet,
+    basename="cidade"
+)
+
+router.register(
+    "cadastro",
+    CadastroViewSet,
+    basename="cadastro"
+)
 
 urlpatterns = [
     path("", include(router.urls)),
